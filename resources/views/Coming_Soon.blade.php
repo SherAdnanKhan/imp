1ed5414349a902aac3c434b1df0a349872b9f49a
Sher Adnan khan <sheradnankhan@gmail.com>
2020-10-30 17:36:24 +0500

    dash board added

--- resources/views/Coming_Soon.blade.php
+++ resources/views/Coming_Soon.blade.php
@@ -5,10 +5,20 @@
                             <div class="row">
                                 <div class="col-12">
                                     <div class="card">
-                                        <div class="card-body">
-<h3 style="text-align: center; color:#5b626b">    Coming Soon !!!! </h3>
-                                        </div>
-                                    </div>
+                <div class="card-block">
+
+                    <div class="ex-page-content text-center">
+                        <h1 class="text-dark">
+                            <span class="text-danger">5</span><span class="text-success">0</span><span class="text-info">0</span>
+                        </h1>
+                        <h4 class="">Coming Soon</h4><br>
+
+                        <a class="btn btn-info mb-5 waves-effect waves-light" href="{{route('admin')}}"><i class="mdi mdi-home"></i> Back to Dashboard</a>
+                    </div>
+
+                </div>
+            </div>
+                                   
                                     </div>
                                     </div>
                                     </div>
